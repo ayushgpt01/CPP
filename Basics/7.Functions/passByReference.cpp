ecc1a4989002a7aeccbbe21d90a22fd001a7679c
ayushgpt01 <ayushgpt043@gmail.com>
2022-04-02 17:03:14 +0530

    Added Pass by Reference

--- Basics/7.Functions/passByReference.cpp
+++ Basics/7.Functions/passByReference.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+void swap(int &a, int &b) {
+    int temp {a};
+    a = b;
+    b = temp;
+}
+
+void printVector(vector<int> &v) {
+    for(int x : v){
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
 int main() {
-    
+    // Pass by reference for integers
+    int x {10} , y {20};
+    cout << "x : " << x << " ,y: " << y << endl;
+    swap(x,y);
+    cout << "x : " << x << " ,y: " << y << endl;
+
+    // pass by reference for vector
+    vector<int> v {20,13,44,2,567,23};
+    printVector(v);
     return 0;
 }
