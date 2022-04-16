d15437afa4a9c821e30dc19efac160023d47a3a1
ayushgpt01 <ayushgpt043@gmail.com>
2022-04-16 16:28:34 +0530

    fix

--- Basics/5.Control Flow/Loops/infiniteLoopBoundaryCase.cpp
+++ Basics/5.Control Flow/Loops/infiniteLoopBoundaryCase.cpp
@@ -6,7 +6,7 @@ int main()
     // At first glance there is nothing wrong with this code but it can result in infinite loop
     float f = 0.2;
     // floats are internally stored as 2.99999 or 3.0000001 so this check fails and it becomes an infinite loop
-    while (f != 3.0)
+    while (f != 3.0) // a better way is to write (f <= 3.0)
     {
         cout << "Y";
         f += 0.2;
