817e69e966ce7933eb1aa7997edcb5434b72f78f
ayushgpt01 <ayushgpt043@gmail.com>
2022-04-28 21:38:37 +0530

    fix

--- DSA using C++/Sorting/InsertionSort.cpp
+++ DSA using C++/Sorting/InsertionSort.cpp
@@ -5,7 +5,7 @@ void insertSort(int a[], int n)
 {
     // Insertion Sort is an in-place and stable algorithm which inserts the key in the presorted array at the right place.
     // In-Place refers to no additional memory allocated during sorting
-    // Stable algorithm is that which sorts in the order of input
+    // Stable algorithm is that which repeated element appears in the same order as input.
     int key, i;
     for (i = 1; i < n; i++)
     {
