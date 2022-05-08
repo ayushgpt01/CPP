01324d1201fa49a0bfb01c31b0d2362373eacc1c
ayushgpt01 <ayushgpt043@gmail.com>
2022-05-08 10:03:08 +0530

    Fixed Linked List and added comments

--- DSA using C++/Data Structures/Linked List/LinkedList.cpp
+++ DSA using C++/Data Structures/Linked List/LinkedList.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Node
 {
-    public:
+public:
     int data;
     Node *next;
 
@@ -26,7 +26,7 @@ class LinkedList
     // Head pointer
     Node *head;
 
-    public:
+public:
     // Constructor to initialize head pointer to null as soon as new list is created
     LinkedList()
     {
@@ -34,19 +34,19 @@ class LinkedList
     }
     void insertBegin(int);
     void insertEnd(int);
-    void insertAfter(int,int);
+    void insertAfter(int, int);
     void printList();
     void deleteNode(int);
-    void insertAt(int x,int i);
+    void insertAt(int x, int i);
 };
 
 // T(n) - O(1)
 void LinkedList::insertBegin(int x)
 {
     // Create a new Node
-    Node* newNode = new Node(x);
+    Node *newNode = new Node(x);
     // Update the new Node to point to first node of list
-    newNode -> next = head;
+    newNode->next = head;
     // update head pointer to point to new Node created
     head = newNode;
 }
@@ -55,64 +55,71 @@ void LinkedList::insertBegin(int x)
 void LinkedList::insertEnd(int x)
 {
     // Create a new node and assign it memory
-    Node* newNode = new Node(x);
+    Node *newNode = new Node(x);
     // check if the list is empty
-    if(head == nullptr)
+    if (head == nullptr)
     {
         // assign head pointer first element and return
         head = newNode;
         return;
     }
     // create a temporary node to transverse the list
-    Node* temp = head;
+    Node *temp = head;
     // transverse until we find the pointer value to be null
-    while(temp->next != nullptr)
+    while (temp->next != nullptr)
     {
         // update the temp value with next pointer to the next node
         temp = temp->next;
     }
     // Connect the newly created node with the last node of the list
-    temp -> next = newNode;
+    temp->next = newNode;
 }
 
-// T(n) - O(n) 
-void LinkedList::insertAfter(int x,int y)
+// T(n) - O(n)
+void LinkedList::insertAfter(int x, int y)
 {
-    Node* newNode = new Node(x);
-    Node* temp = head;
-    while(temp!=nullptr)
+    // create a new node
+    Node *newNode = new Node(x);
+    Node *temp = head;
+    // loop to find the element y in list
+    while (temp != nullptr)
     {
-        if(temp->data = y)
+        if (temp->data = y)
         {
+            // once found we insert the new node after node with y
             newNode->next = temp->next;
             temp->next = newNode;
             return;
         }
         temp = temp->next;
     }
+    cout << y << " not found in the list \n";
 }
 
 // T(n) - O(i)
-void LinkedList::insertAt(int x,int i)
+void LinkedList::insertAt(int x, int i)
 {
-    if(i==1)
+    // if inserting at first position in list
+    if (i == 1)
     {
-        Node* newNode = new Node(x);
+        Node *newNode = new Node(x);
         newNode->next = head;
         head = newNode;
         return;
     }
-    Node* temp = head;
-    for(int j=2;j<i;j++)
+    Node *temp = head;
+    // iterate to position where we need to insert data
+    for (int j = 2; j < i; j++)
     {
-        if(temp==nullptr)
+        if (temp == nullptr)
         {
             cout << "List too short\n";
             return;
         }
         temp = temp->next;
     }
-    Node* newNode = new Node(x);
+    // insert the node
+    Node *newNode = new Node(x);
     newNode->next = temp->next;
     temp->next = newNode;
 }
@@ -120,35 +127,37 @@ void LinkedList::insertAt(int x,int i)
 // T(n) - O(n)
 void LinkedList::deleteNode(int x)
 {
-    Node* p;
-    if(head->data==x)
+    Node *p;
+    // Delete the first item
+    if (head->data == x)
     {
         p = head;
-        head = head -> next;
-        delete(p);
+        head = head->next;
+        delete (p);
         cout << "Item deleted\n";
         return;
     }
-    Node* temp = head;
-    while(temp->next!=nullptr)
+    Node *temp = head;
+    // loop to iterate to item x and delete item
+    while (temp->next != nullptr)
     {
-        if(temp->next->data = x)
+        if (temp->next->data = x)
         {
             p = temp->next;
             temp->next = temp->next->next;
-            delete(p);
+            delete (p);
             cout << "Item deleted\n";
             return;
         }
-        temp=temp->next;
+        temp = temp->next;
     }
 }
 
 // T(n) - O(n)
 void LinkedList::printList()
 {
-    Node* temp = head;
-    while(temp!=nullptr)
+    Node *temp = head;
+    while (temp != nullptr)
     {
         cout << temp->data << " ";
         temp = temp->next;
@@ -156,21 +165,22 @@ void LinkedList::printList()
     cout << endl;
 }
 
-int main() {
+int main()
+{
     LinkedList list;
     list.insertBegin(5);
     list.printList();
     list.insertEnd(15);
     list.printList();
-    list.insertAt(65,2);
+    list.insertAt(65, 2);
     list.printList();
-    list.insertAt(63,1);
+    list.insertAt(63, 1);
     list.printList();
     list.insertBegin(19);
     list.printList();
-    list.insertAfter(14,19);
+    list.insertAfter(14, 19);
     list.printList();
-    list.insertAt(18,4);
+    list.insertAt(18, 4);
     list.printList();
     list.deleteNode(19);
     list.printList();
