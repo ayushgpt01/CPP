b27fbad512a1e4be2d475d931b41b90fdd9bb3ff
ayushgpt01 <ayushgpt043@gmail.com>
2022-05-13 16:04:59 +0530

    Fixed Heap

--- DSA using C++/Data Structures/Heap/heap.cpp
+++ DSA using C++/Data Structures/Heap/heap.cpp
@@ -1,110 +1,40 @@
 #include<iostream>
 using namespace std;
 
-class maxheap
+void heapify(int arr[],int n,int i)
 {
-    int *arr;
-    int capacity;
-    int curr_size;
-public:
-    maxheap(int n)
-    {
-        capacity = n;
-        curr_size = 0;
-        arr = new int[capacity];
-    }
-
-    int parent(int i){return (i-1)/2;}
-    int left(int i){return ((2*i)+1);}  // for array starting with 1 = 2i
-    int right(int i){return (2*i + 2);}  // arr[1..n]  = 2i + 1
-    void heapify(int);
-    void insert(int);
-    void removeIndex(int);
-    int removeMax();
-    void print();
-};
-
-int main() {
-    maxheap h(15);
-    h.insert(20);
-    h.insert(2);
-    h.insert(7);
-    h.insert(33);
-    h.insert(1);
-    h.print();
-    return 0;
-}
-
-void maxheap::print()
-{
-    for(int i=0;i<curr_size;i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-void maxheap::insert(int data)
-{
-    if(curr_size == capacity)
-    {
-        cout << "Heap is full. Cannot insert\n";
-        return;
-    }
-    // Insert the data at the last place in array
-    int i = curr_size;
-    curr_size++;
-    arr[i] = data;
-    int par = parent(i);
-    // Loop to check from current inserted node to root if it satisfies max heap property or not
-    while(i!=0 && arr[par] < arr[i])
-    {
-        swap(arr[par],arr[i]);
-        i = par;
-        par = parent(i);
-    }
-}
-
-void maxheap::removeIndex(int i)
-{
-    arr[i] = INT_MAX;
-    int par = parent(i);
-    while(i!=0 && arr[par] < arr[i])
-    {
-        swap(arr[par],arr[i]);
-        i = par;
-        par = parent(i);
-    }
-    heapify(0);
-}
-
-int maxheap::removeMax()
-{
-    if(curr_size <= 0)
-        return INT_MIN;
-    if(curr_size == 1)
-    {
-        curr_size--;
-        return arr[0];
-    }
-    int root = arr[0];
-    arr[0] = arr[curr_size-1];
-    heapify(0);
-    return root;
-}
-
-void maxheap::heapify(int i)
-{
-    int l = left(i);
-    int r = right(i);
+    int l = (2*i)+1;
+    int r = (2*i)+2;
     int largest {i};
-    if (l <= capacity && arr[l] > arr[i])
+    if (l < n && arr[l] > arr[largest])
         largest = l;
-    if (r <= capacity && arr[r] > arr[i])
+    if (r < n && arr[r] > arr[largest])
         largest = r;
     if(largest != i)
     {
         swap(arr[i],arr[largest]);
-        heapify(largest);
+        heapify(arr,n,largest);
     }
+}
+
+void buildHeap(int arr[],int n)
+{
+    for(int i=(n-1)/2;i>=0;i--)
+        heapify(arr,n,i);
+}
+
+void print(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] {3,5,1,7,8,4,2,9,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    print(arr,n);
+    buildHeap(arr,n);
+    print(arr,n);
+    return 0;
 }
